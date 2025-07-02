5f2e19b6b26c961e134817429f742716f6696005
Sohee10ow <sh663746@gmail.com>
2025-07-02 22:32:47 +0900

    Initial Commit

---
+++ help_ai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  help_ai
+//
+//  Created by 임또이 on 7/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ help_ai/help_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  help_aiApp.swift
+//  help_ai
+//
+//  Created by 임또이 on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct help_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
